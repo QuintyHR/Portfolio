86a6f2858d4dc3852a6fa42851b9a8068829d1c3
QuintyHR <70649864+QuintyHR@users.noreply.github.com>
2022-05-20 15:45:36 +0200

    Big updates on CSS for Mobile and added a subnav

---
+++ js/script.js
@@ -0,0 +1,159 @@
+// About button
+const aboutButton = document.querySelector("#about-button")
+const aboutSection = document.querySelector("#about")
+
+if(aboutButton){
+    aboutButton.addEventListener("click", function(){
+        aboutSection.scrollIntoView({behavior: 'smooth'});
+    })
+}
+
+// Study button
+const studyButton = document.querySelector("#study-button")
+const studySection = document.querySelector("#study")
+
+if(studyButton){
+    studyButton.addEventListener("click", function(){
+        studySection.scrollIntoView({behavior: 'smooth'});
+    })
+}
+
+// Projects button
+const projectsButton = document.querySelector("#projects-button")
+const projectsSection = document.querySelector("#projects")
+
+if(projectsButton){
+    projectsButton.addEventListener("click", function(){
+        projectsSection.scrollIntoView({behavior: 'smooth'});
+    })
+}
+
+// More button
+const moreButton = document.querySelector("#more-button")
+const moreSection = document.querySelector("#more")
+
+if(moreButton){
+    moreButton.addEventListener("click", function(){
+        moreSection.scrollIntoView({behavior: 'smooth'});
+    })
+}
+
+// Font-Back button
+const frontBackButton = document.querySelector("#front-back-button")
+const frontBackSection = document.querySelector("#front-back")
+
+if(frontBackButton){
+    frontBackButton.addEventListener("click", function(){
+        frontBackSection.scrollIntoView({behavior: 'smooth'});
+    })
+}
+
+// Webshop button
+const webshopButton = document.querySelector("#webshop-button")
+const webshopSection = document.querySelector("#webshop-back")
+
+if(webshopButton){
+    webshopButton.addEventListener("click", function(){
+        webshopSection.scrollIntoView({behavior: 'smooth'});
+    })
+}
+
+// Game button
+const gameButton = document.querySelector("#game-button")
+const gameSection = document.querySelector("#game-back")
+
+if(gameButton){
+    gameButton.addEventListener("click", function(){
+        gameSection.scrollIntoView({behavior: 'smooth'});
+    })
+}
+
+// Laravel button
+const laravelButton = document.querySelector("#laravel-button")
+const laravelSection = document.querySelector("#laravel-back")
+
+if(laravelButton){
+    laravelButton.addEventListener("click", function(){
+        laravelSection.scrollIntoView({behavior: 'smooth'});
+    })
+}
+
+// ML button
+const mlButton = document.querySelector("#ml-button")
+const mlSection = document.querySelector("#ml-back")
+
+if(mlButton){
+    mlButton.addEventListener("click", function(){
+        mlSection.scrollIntoView({behavior: 'smooth'});
+    })
+}
+
+// Mask button
+const maskButton = document.querySelector("#mask-button")
+const maskSection = document.querySelector("#mask-back")
+
+if(maskButton){
+    maskButton.addEventListener("click", function(){
+        maskSection.scrollIntoView({behavior: 'smooth'});
+    })
+}
+
+// Finger button
+const fingerButton = document.querySelector("#finger-button")
+const fingerSection = document.querySelector("#finger-back")
+
+if(fingerButton){
+    fingerButton.addEventListener("click", function(){
+        fingerSection.scrollIntoView({behavior: 'smooth'});
+    })
+}
+
+// Hand button
+const handButton = document.querySelector("#hand-button")
+const handSection = document.querySelector("#hand-back")
+
+if(handButton){
+    handButton.addEventListener("click", function(){
+        handSection.scrollIntoView({behavior: 'smooth'});
+    })
+}
+
+// Diabetes button
+const diabetesButton = document.querySelector("#diabetes-button")
+const diabetesSection = document.querySelector("#diabetes-back")
+
+if(diabetesButton){
+    diabetesButton.addEventListener("click", function(){
+        diabetesSection.scrollIntoView({behavior: 'smooth'});
+    })
+}
+
+// Emission button
+const emissionButton = document.querySelector("#emission-button")
+const emissionSection = document.querySelector("#emission-back")
+
+if(emissionButton){
+    emissionButton.addEventListener("click", function(){
+        emissionSection.scrollIntoView({behavior: 'smooth'});
+    })
+}
+
+// Extra button
+const extraButton = document.querySelector("#extra-button")
+const extraSection = document.querySelector("#extra-back")
+
+if(extraButton){
+    extraButton.addEventListener("click", function(){
+        extraSection.scrollIntoView({behavior: 'smooth'});
+    })
+}
+
+// Route button
+const routeButton = document.querySelector("#route-button")
+const routeSection = document.querySelector("#route-back")
+
+if(routeButton){
+    routeButton.addEventListener("click", function(){
+        routeSection.scrollIntoView({behavior: 'smooth'});
+    })
+}
